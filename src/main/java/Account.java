50125c2414d47c463f4d4e5002a8b8e4556e80bf
marcindluzewski <102620318+marcindluzewski@users.noreply.github.com>
2022-04-26 18:48:52 +0200

    add new method : createAccount

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Account {
    private String name;
    private String surname;
    private String accNumber;
    private long balance;
+
+   Scanner scanner = new Scanner(System.in);
 
    public String getName(){
        return name;
@@ -13,10 +17,27 @@ public class Account {
    public String getAccNumber(){
        return accNumber;
    }
+    public long getBalance() {
+        return balance;
+    }
 
-   public void setName(String name){
+    public void setName(String name){
        this.name = name;
    }
-   public void setSurname(String surname){this.surname = surname;}
-   public void setAccNumber(String accNumber){this.accNumber = accNumber;}
+    public void setSurname(String surname){this.surname = surname;}
+    public void setAccNumber(String accNumber){this.accNumber = accNumber;}
+    public void setBalance(long balance) {this.balance = balance;}
+
+    public void createAccount(){
+        System.out.println("Hello, please enter your name");
+        name = scanner.next();
+        System.out.println("Enter your surname");
+        surname = scanner.next();
+        System.out.println("Please enter your account number");
+        accNumber = scanner.next();
+        System.out.println("Enter your balance");
+        balance = scanner.nextLong();
+
+    }
+
 }
--- src/main/java/BankApp.java
+++ src/main/java/BankApp.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class BankApp {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        Account account = new Account();
+
+        account.createAccount();
     }
 }
